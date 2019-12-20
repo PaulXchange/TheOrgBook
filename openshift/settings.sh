ae237b5cce8e07d4bd457d2c5c8422bcdd77b02f
paulxchange <paulraymondagada@gmail.com>
2019-12-20 11:39:13 +0800

    Openshift

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,10 +1,10 @@
-export PROJECT_NAMESPACE="devex-von"
+export PROJECT_NAMESPACE="RegovOrg"
 
 # The templates that should not have their GIT referances(uri and ref) over-ridden
 # Templates NOT in this list will have they GIT referances over-ridden
 # with the values of GIT_URI and GIT_REF
 export skip_git_overrides="schema-spy-build.json solr-base-build.json backup-build.json"
-export GIT_URI="https://github.com/bcgov/TheOrgBook.git"
+export GIT_URI="https://github.com/PaulXchange/TheOrgBook"
 export GIT_REF="master"
 
 # The builds to be triggered after buildconfigs created (not auto-triggered)
